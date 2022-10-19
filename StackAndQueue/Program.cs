c8cb3934e317d923b7ee3daaec66c64542884433
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-19 17:25:36 +0530

    Adding Elements into Stack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackLinkedList stack = new StackLinkedList();
+            Console.WriteLine("Pushing Elements in the Stack as :\n");
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\nElement in stack given as :\n");
+            stack.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackAndQueue/StackLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class StackLinkedList
+    {
+        Node top;
+
+
+        public StackLinkedList()
+        {
+            top = null;
+        }
+        public void Push(int data)//Method to add elements in stack
+        {
+            Node nodeObj = new Node(data);
+            nodeObj.Next = top;
+            top = nodeObj;
+            Console.WriteLine("{0} Push to the stack", data);
+        }
+        public void Display()
+        {
+            if (top == null)
+                Console.WriteLine("Stack is Empty here");
+
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
