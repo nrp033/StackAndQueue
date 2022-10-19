aa07a4dffee8c054f8420926f5f4a1d1503982b2
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-19 18:12:16 +0530

    Creating Queue of elements using linked List and adding Element as 56, 30, 70 and 56 will be on top of the stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -9,8 +9,11 @@
             {
                 Console.WriteLine("\n\n****Choose From the Following Options****\n\n");
                 Console.WriteLine("\n1) Insert And Diplay Element Into Stack\n" +
-                    "2) Insert and Peek , Pop Elements from Stack Until Stack Become Empty");
+                   "2) Insert and Peek , Pop Elements from Stack Until Stack Become Empty\n" +
+                   "3) Insert and Display Element into Queue");
                 StackLinkedList stack = new StackLinkedList();
+                QueueLinkedList queue = new QueueLinkedList();
+                Console.Write("\n\nEnter Your Choice : ");
                 int option = int.Parse(Console.ReadLine());
                 switch (option)
                 {
@@ -34,6 +37,17 @@
                         stack.Display();
                         stack.IsEmpty();
                         stack.Display();
+                        break;
+
+
+                    case 3:
+
+                        Console.WriteLine("Adding elements in the Queue :");
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        Console.WriteLine("\nQueue Contains :  \n");
+                        queue.Display();
                         break;
 
 
---
+++ StackAndQueue/QueueLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class QueueLinkedList
+    {
+        //In Queue front and rear Nodes are present
+        Node front;
+        Node rear;
+
+
+        //Enqueue method is Used to Add elements in the queue
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear = rear.Next;
+            }
+            Console.WriteLine("{0} Inserted into Queue", newnode.data);
+
+        }
+
+        //Display method is used to display elements in queue
+
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Node temp = front;
+            while (temp != null)
+            {
+                Console.Write(temp.data+" -> "  );
+                temp = temp.Next;
+            }
+        }
+    }
+}
